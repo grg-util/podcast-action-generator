23413c994f67d560dbf32bf5cd997066cdb2d1d7
George Mathew <49146450+grgemat@users.noreply.github.com>
2025-04-17 04:58:09 +0000

    updated entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,10 +3,10 @@
 echo "============================================"
 
 git config --global user.name "${GITHUB_ACTOR}"
-git config --global user.email "${IMPUT_EMAIL}"
-git config --global -add safe.directory /github/workspace
+git config --global user.email "${INPUT_EMAIL}"
+git config --global --add safe.directory /github/workspace
 
-python3 usr/bin/feed.py
+python3 /usr/bin/feed.py
 
 git add -A && git commit -m "Update Feed" 
 git push --set-upstream origin main 
